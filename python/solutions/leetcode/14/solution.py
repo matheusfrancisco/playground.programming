9fb28a43d518a1541f8d884a30ac7da7381cee77
matheusfrancisco <matheusmachadoufsc@gmail.com>
2023-09-26 22:51:27 -0300

    add: leetcode 14 divide and conquer

--- python/solutions/leetcode/14/solution.py
+++ python/solutions/leetcode/14/solution.py
@@ -9,11 +9,32 @@ def longest_common_prefix(strs):
                 return strs[0][0:i]
     return ""
 
+def common_prefix(s1, s2):
+    i = 0
+    while i < len(s1) and i < len(s2) and s1[i] == s2[i]:
+        i += 1
+    return s1[0:i]
+
+def lcp_divide_and_conquer_helper(strs, l, r):
+    if l == r:
+        return strs[l]
+    else:
+        mid = (l + r) // 2
+        lcp_left = lcp_divide_and_conquer_helper(strs, l, mid)
+        lcp_right = lcp_divide_and_conquer_helper(strs, mid + 1, r)
+        return common_prefix(lcp_left, lcp_right)
+
+def lcp_divide_and_conquer(strs):
+    if len(strs) == 0:
+        return ""
+    return lcp_divide_and_conquer_helper(strs, 0, len(strs) - 1)
+
+
 def main():
     n = int(input())
     for i in range(n):
         strs = input().split(" ")
-        out = longest_common_prefix(strs)
+        out = lcp_divide_and_conquer(strs)
         print(f"{out}")
 
 main()
