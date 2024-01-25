72259fbb572459370e64ce47ce4cae758b8c25a6
matheusfrancisco <matheusmachadoufsc@gmail.com>
2024-01-25 09:55:31 -0300

    fix

--- python/solutions/leetcode/494/solution.py
+++ python/solutions/leetcode/494/solution.py
@@ -1,21 +1,22 @@
 
 
 def main():
-    s, target = input().split(" ")
+    target = int(input())
     dp = {}
     nums = input().split(" ")
     nums = list(map(int, nums))
+
     def backtracking(nums, i, t):
         if (i, t) in dp:
             return dp[(i, t)]
         if i == len(nums):
-            if t ==  target:
+            if t ==  0:
                 return 1
             return 0
         dp[(i, t)] = backtracking(nums, i + 1, t - nums[i]) + backtracking(nums, i + 1, t + nums[i])
         return dp[(i, t)]
-
-    return backtracking(nums, 0,  int(target))
+    r = backtracking(nums, 0, target)
+    return r
 
 
 print(main())
