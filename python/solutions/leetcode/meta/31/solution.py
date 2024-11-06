57433373fc337f240a4575aab4bc00024cbc1a88
matheusfrancisco <matheusmachadoufsc@gmail.com>
2024-11-06 20:26:44 -0300

    add next permutation

---
+++ python/solutions/leetcode/meta/31/solution.py
@@ -0,0 +1,22 @@
+
+from typing import List
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        pivot = None
+        for i in range(len(nums) - 1, 0, -1):
+            if nums[i] > nums[i-1]:
+                pivot = i - 1
+                break
+        else:
+            nums.reverse()
+            return
+        swap = len(nums) - 1
+        while nums[swap] <= nums[pivot]:
+            swap -= 1
+
+        nums[pivot], nums[swap] = nums[swap], nums[pivot]
+        nums[pivot+1:] = reversed(nums[pivot+1:])
