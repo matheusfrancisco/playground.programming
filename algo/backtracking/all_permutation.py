057cb3f424981b4be1bd0be4103636b92719485e
matheusfrancisco <matheusmachadoufsc@gmail.com>
2024-11-08 07:24:27 -0300

    generate parentheses

---
+++ algo/backtracking/all_permutation.py
@@ -0,0 +1,65 @@
+"""
+Given a string of unique letters, find all of its distinct permutations.
+
+Permutation means arranging things with an order. 
+For example, permutations of [1, 2] are [1, 2] and [2, 1]. 
+Permutations are best visualized with trees.
+"""
+
+from typing import List
+
+def permutations(letters: str) -> List[str]:
+
+    res = []
+    path = []
+    used = [False] * len(letters)
+
+    def dfs(start):
+        if start == len(letters):
+            res.append(''.join(path))
+            return
+        for i, letter in enumerate(letters):
+            if used[i]:
+                continue
+            path.append(letter)
+            used[i] = True
+            dfs(start + 1)
+            path.pop()
+            used[i] = False
+    dfs(0)
+    return res
+
+
+print(permutations('abc'))  # ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
+# (out) ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
+# abc
+# dfs(0)
+#    i = 0, letter = a
+#    path = ['a']
+#    used = [True, False, False]
+#    dfs(1)
+#       i = 0, letter = b
+#       path = ['a', 'b']
+#       used = [True, True, False]
+#       dfs(2)
+#          i = 0, letter = c
+#          path = ['a', 'b', 'c']
+#          used = [True, True, True]
+#          dfs(3)
+#             reach base case, append 'abc' to res
+#             res = ['abc']
+#          path = ['a', 'b']
+#          used = [True, True, False]
+#       path = ['a']
+#       used = [True, False, False]
+#       dfs(2)
+#          i = 1, letter = c
+#          path = ['a', 'c']
+#          used = [True, False, True]
+#          dfs(3)
+#             reach base case, append 'acb' to res
+#             res = ['abc', 'acb']
+#          path = ['a']
+#          used = [True, False, False]
+#    path = []
+# ...
--- algo/backtracking/generate_parentheses.py
+++ algo/backtracking/generate_parentheses.py
@@ -6,5 +6,24 @@ each opening parenthesis has matching closing parentheses.
 For example, () is a valid string but )( is not a valid string because ) has no matching parenthesis before it and ( has no matching parenthesis after it.
 """
 
+from typing import List
 
+def generate_parentheses(n: int) -> List[str]:
+    res = []
+    path = []
 
+    def dfs(start, open_count, close_count):
+        if start == 2 * n:
+            res.append(''.join(path))
+            return
+        if open_count < n:
+            path.append('(')
+            dfs(start + 1, open_count + 1, close_count)
+            path.pop()
+        if close_count < open_count:
+            path.append(')')
+            dfs(start + 1, open_count, close_count + 1)
+            path.pop()
+
+    dfs(0, 0, 0)
+    return res
