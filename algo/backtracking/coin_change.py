fb9d660007aa147427a75ce25817d1b6c75b0a1b
matheusfrancisco <matheusmachadoufsc@gmail.com>
2024-11-09 11:29:26 -0300

    coin change

--- algo/backtracking/coin_change.py
+++ algo/backtracking/coin_change.py
@@ -9,16 +9,19 @@ def min_coins(coins, amount, s, memo):
         return float('inf')
 
     if memo[s] != -1:
+        print(f"memo hit {s} -> {memo[s]}")
         return memo[s]
 
     ans = float('inf')
     for coin in coins:
-        print(coin, amount, s + coin, memo)
+        print(f"{s} -> {s+coin}", amount, memo)
         r = min_coins(coins, amount, s + coin, memo)
+        print(f"result from {s}", r)
         if r == float('inf'):
             continue
-        print("r:", 1 + r)
+        print(f"{ans}, {1 + r}")
         ans = min(ans, 1 + r)
+        print(f"{s} ->  {s + coin}: r {ans}", memo)
 
     memo[s] = ans
     return ans
@@ -30,4 +33,16 @@ def coin_change(coins, amount):
     return result if result != float('inf') else -1
 
 
-print(coin_change([1, 2, 5], 11))  # 3
+def coin_change_2(coins, amount):
+    dp = [float("inf")] * (amount + 1)
+    dp[0] = 0
+    for a in range(1, amount + 1):
+        for c in coins:
+            if a - c >= 0:
+                dp[a] = min(dp[a], dp[a - c] + 1)
+
+    return dp[amount] if dp[amount] != amount + 1 else -1
+
+
+print(coin_change_2([1, 2, 5], 11))  # 3
+print(coin_change([1, 2, 5], 3))  # 3
