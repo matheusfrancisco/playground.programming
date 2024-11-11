4d0ab6570675773f171ca254a58873ff15446669
matheusfrancisco <matheusmachadoufsc@gmail.com>
2024-11-11 19:18:11 -0300

    add: 721 accounts merge

---
+++ python/solutions/leetcode/meta/721/solution.py
@@ -0,0 +1,37 @@
+from typing import List
+import collections
+
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        graph = collections.defaultdict(set)
+        email_to_account = {}
+        for acc in accounts:
+            name = acc[0]
+            for email in acc[1:]:
+                print(email, acc[1])
+                graph[email].add(acc[1])
+                graph[acc[1]].add(email)
+                email_to_account[email] = name
+
+        res = []
+        visited = set()
+
+        for email in graph:
+            if email not in visited:
+                stack = [email]
+                visited.add(email)
+                local_res = []
+                while stack:
+                    node = stack.pop()
+                    local_res.append(node)
+                    for edge in graph[node]:
+                        if edge not in visited:
+                            stack.append(edge)
+                            visited.add(edge)
+                res.append([email_to_account[email]] + sorted(local_res))
+        return res
+
+
+acc = [["John", "johnsmith@mail.com", "john_newyork@mail.com"], ["John", "johnsmith@mail.com",
+                                                                 "john00@mail.com"], ["Mary", "mary@mail.com"], ["John", "johnnybravo@mail.com"]]
+print(Solution().accountsMerge(acc))
