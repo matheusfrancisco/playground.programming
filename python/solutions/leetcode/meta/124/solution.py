d73dccd702e63116e769167215494f5cb3993d47
matheusfrancisco <matheusmachadoufsc@gmail.com>
2024-11-14 07:58:38 -0300

    add some problems lca ii

--- python/solutions/leetcode/meta/124/solution.py
+++ python/solutions/leetcode/meta/124/solution.py
@@ -1,5 +1,9 @@
 from typing import Optional
 # Definition for a binary tree node.
+# max = -10
+# max = 9
+# max = 15
+# max = 42
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -38,7 +42,19 @@ class Solution:
             node.val + l_path,
             node.val + r_path)
 
-# max = -10
-# max = 9
-# max = 15
-# max = 42
+class Solution2:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        out = [root.val]
+
+        def dfs(root):
+            if not root:
+                return 0
+            left = dfs(root.left)
+            right = dfs(root.right)
+            left = max(left, 0)
+            right = max(right, 0)
+            out[0] = max(out[0], left+right+root.val)
+
+            return max(left, right) + root.val
+        dfs(root)
+        return out[0]
