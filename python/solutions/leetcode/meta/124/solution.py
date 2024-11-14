51020a750fe63479f590c3ad19464a3a439a470d
matheusfrancisco <matheusmachadoufsc@gmail.com>
2024-11-13 21:49:32 -0300

    add 124 binary tree max path sum

---
+++ python/solutions/leetcode/meta/124/solution.py
@@ -0,0 +1,44 @@
+from typing import Optional
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        self.max_path_sum = root.val
+        self.dfs(root)
+
+        return self.max_path_sum
+
+    def dfs(self, node):
+        if not node:
+            return 0
+
+        if not node.left and not node.right:
+            self.max_path_sum = max(self.max_path_sum, node.val)
+            return node.val
+
+        l_path = self.dfs(node.left)
+        r_path = self.dfs(node.right)
+
+        self.max_path_sum = max(
+            self.max_path_sum,
+            node.val,
+            node.val + l_path + r_path,
+            node.val + l_path,
+            node.val + r_path)
+        return max(
+            node.val,
+            0,
+            node.val + l_path,
+            node.val + r_path)
+
+# max = -10
+# max = 9
+# max = 15
+# max = 42
