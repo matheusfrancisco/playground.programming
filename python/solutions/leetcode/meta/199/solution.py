49cb40cd13b5788d6c7ef5d44e503397dce37aa2
matheusfrancisco <matheusmachadoufsc@gmail.com>
2024-11-16 09:01:51 -0300

    add 199 bt right side view

--- python/solutions/leetcode/meta/199/solution.py
+++ python/solutions/leetcode/meta/199/solution.py
@@ -1,3 +1,5 @@
+from typing import Optional, List
+import collections
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -28,3 +30,26 @@ def rightSideView(root):
 
 
 print(rightSideView(t) == [1, 3, 4])
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution2:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        q = collections.deque([root])
+        res = []
+        while q:
+            level_length = len(q)
+            for i in range(level_length):
+                node = q.popleft()
+                if i == level_length - 1:
+                    res.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+        return res
