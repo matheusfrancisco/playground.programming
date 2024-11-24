47966a475fed132a4b15aefb513fcd7c2f8e0b37
matheusfrancisco <matheusmachadoufsc@gmail.com>
2024-11-24 15:27:31 -0300

    add solution 670

--- python/solutions/leetcode/meta/670/solution.py
+++ python/solutions/leetcode/meta/670/solution.py
@@ -1,4 +1,6 @@
-class Solution:
+import collections
+
+class Solution1:
     def maximumSwap(self, num: int) -> int:
         # 2789 -> 9782
         # 9919 -> 9991
@@ -29,3 +31,41 @@ class Solution:
         n[swapj], n[swapi] = n[swapi], n[swapj]
 
         return int("".join(n))
+
+class Solution:
+    def maximumSwap(self, num: int) -> int:
+        if num <= 11:
+            return num
+        num_as_arr = collections.deque([])
+
+        while num:
+            num_as_arr.appendleft(num % 10)
+            num //= 10
+        max_seen = -1
+        max_seen_at = len(num_as_arr)
+
+        i = len(num_as_arr) - 1
+        while i >= 0:
+            cur_num = num_as_arr[i]
+            # print(i,num_as_arr)
+            num_as_arr[i] = (cur_num, max_seen, max_seen_at)
+            # print(i,num_as_arr)
+            if cur_num > max_seen:
+                max_seen = cur_num
+                max_seen_at = i
+            i -= 1
+
+        i = 0
+        while i < len(num_as_arr):
+            cur_num, max_to_right, max_seen_at = num_as_arr[i]
+            if max_to_right > cur_num:
+                num_as_arr[i], num_as_arr[max_seen_at] = num_as_arr[max_seen_at], num_as_arr[i]
+                break
+            i += 1
+
+        num = 0
+        for cur_num, _, _ in num_as_arr:
+            num = num * 10 + cur_num
+        return num
+# O(N), n number of digits in number
+# O(N), n number of digits
