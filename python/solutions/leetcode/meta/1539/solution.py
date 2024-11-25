167a4c2c13dea8bb79bca6c009e12d42da55a5f0
matheusfrancisco <matheusmachadoufsc@gmail.com>
2024-11-24 22:36:17 -0300

    add 1539 problem

--- python/solutions/leetcode/meta/1539/solution.py
+++ python/solutions/leetcode/meta/1539/solution.py
@@ -1,5 +1,27 @@
 
+from typing import List
 
+class Solution:
+    def findKthPositive(self, arr: List[int], k: int) -> int:
+        # k=7
+        # [4,5,6]
+        if arr[0] != 1:
+            if arr[0] - 1 >= k:
+                return k
+            else:
+                k -= arr[0] - 1
+        i = 0
+        while i < len(arr) - 1:
+            diff = arr[i + 1] - arr[i]
+            if diff != 1:
+                for num in range(arr[i] + 1, arr[i+1]):
+                    k -= 1
+                    if not k:
+                        return num
+            i += 1
+
+        if k:
+            return arr[-1] + k
 
 
 # arr = [2, 3, 4, 7, 11]
